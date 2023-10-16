87f4e0268b5ba79c032ad01d2b2c20426d535391
Afonso Ramon de Araujo Teles <afonso1907@hotmail.com>
2023-10-16 19:21:50 -0300

    Starting base project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import './styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className='dark' lang="en">
+      <body className={` dark:bg-slate-800 ${inter.className}`}>
+        {children}
+      </body>
     </html>
   )
 }
